1b37578098f96986a3aedf07a6ba3c2aee1b6719
Tobias Breitkopf <tobias.breitkopf@googlemail.com>
2023-11-03 08:30:26 +0100

    Adjusted Sidendpoint to be able to use normal links

--- server/server.py
+++ server/server.py
@@ -7,16 +7,25 @@ app = Flask(__name__)
 app.config["DEBUG"] = log_level == logging.DEBUG
 logging.basicConfig(level = log_level)
 
+def StripHtmlOff(instring):
+    if(".html" in instring):
+        return str(instring).split(".")[0]
+    return str(instring)
+
 @app.route("/")
 def idex():
     return render_template("index.html")
 
 @app.route("/<side>")
 def side(side = ""):
+    side = StripHtmlOff(side)
+    print(f"Requested Side: {side}")
     if(side == ""):
         return render_template("index.html")
     if(os.path.isfile(f"./templates/{side}.html")):
+        print("Sucseeded")
         return render_template(f"{side}.html")
+    print("Failed")
     resp = Response("Side Not Found", status = 404)
     return resp
 
